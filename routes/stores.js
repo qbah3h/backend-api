1191cf7c6be9751726f6b41ea388c7039005fc93
Heikel <heikel.gonzalez@oracle.com>
2022-05-22 16:29:15 -0300

    Refactor and test on Heroku

--- routes/stores.js
+++ routes/stores.js
@@ -65,7 +65,7 @@ router.route('/stores')
   .post(function (req, res, next) {
     logger.info("post pending validations")
     //similar login validator as get, can be refactored as a midleware
-    console.log()
+    console.log(req.body)
     next()
   },
     function (req, res) {
